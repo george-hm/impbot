d556c980cefc49f47620c8985804b8e76b7551a0
Imp <georgehm@pm.me>
2019-03-23 16:47:05 +0000

    Update main.js to pass and require correct values

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ const modDiscord = require("discord.js");
 const bot = new modDiscord.Client();
 const fs = require("fs");
 const objConfig = JSON.parse(fs.readFileSync("./config.json", "utf8"));
-const modHandler = require("handler.js");
+const modHandler = require("./handler.js");
 
 bot.on("ready", () => {
 	console.log("logged in as", bot.user.tag);
-	bot.user.setActivity(prefix,"help");
+	bot.user.setActivity(objConfig.prefix,"help");
 });
 
 bot.on("message", objMsg => {
-	modHandler.find(objMsg, objMsg, bot);
+	modHandler.find(objConfig.prefix, objMsg, bot);
 	//TODO handle commands
 });
 
