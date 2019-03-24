d3ef185e40629351c153c1c8af179c510f6afcae
Imp <georgehm@pm.me>
2019-03-24 13:59:41 +0000

    Update ping.js to return error properly

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,13 +2,13 @@
  * Ping the bot
  *
  * @param      {Object}   context  {objMsg, bot}
- * @return     {Promise}  { description_of_the_return_value }
+ * @return     {Promise}  
  */
 module.exports.ping = (context) => {
 	return new Promise((resolve, reject) => {
-		if (!context.objMsg || !context.bot) {
-			reject("help")
-		}
+		if (!context.objMsg || !context.bot)
+			return reject("help")
+
 		return resolve(context.objMsg.reply("Pong! Response time: " + context.bot.ping.toFixed(0) + "ms"));
 	});
 }
