449b450ed838b3a8608f555ae5358b94ab871450
Imp <georgehm@pm.me>
2019-03-24 14:00:21 +0000

    Add uptime.js

---
+++ src/commands/uptime.js
@@ -0,0 +1,15 @@
+/**
+ * Return uptime of the bot
+ *
+ * @param      {Object}   context  { objMsg, bot }
+ * @return     {Promise}  
+ */
+module.exports.uptime = (context) => {
+	return new Promise((resolve, reject) => {
+		if (!context.objMsg || !context.bot)
+			return reject("help");
+
+		let intUptime = (context.bot.uptime/1000).toFixed(0);
+		return resolve(context.objMsg.reply("Uptime: " + intUptime + " seconds."));
+	});
+}
