2d9cca2938c0be508b11ed9e43ff7dc5ec8199b0
Imp <georgehm@pm.me>
2019-03-24 13:59:25 +0000

    Add (hopefully) better way to export commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,3 +1,12 @@
-const modPing = require("./ping.js");
+const uptime = require("./uptime");
+const ping   = require("./ping");
 
-module.exports.ping = modPing.ping
+let objComands = {
+	"uptime": uptime,
+	"ping"  : ping
+}
+
+for (let command in objComands) {
+	// e.g. module.exports.ping = objComands.ping.ping
+	module.exports[command] = objComands[command][command]
+}
