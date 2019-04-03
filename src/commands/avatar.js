7afb9f5a31554c315def3d8bdc3734f36f185efa
Imp <georgehm@pm.me>
2019-04-03 08:01:32 +0100

    Add error handling for avatar command

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -9,10 +9,17 @@ module.exports.avatar = (context) => {
 			return resolve(context.objMsg.reply(strAvatarURL));
 		}
 		else {
+			// get user to acquire avatar url
 			context.bot.fetchUser(context.target).then(user => {
+				if (!user.avatarURL) {
+					return resolve(context.objMsg.reply("That user does not have a profile picture."))
+				}
 				strAvatarURL = user.avatarURL;
 				return resolve(context.objMsg.reply(strAvatarURL));
 			}).catch(err => {
+				if (err.message === "Unknown User") {
+					return resolve(context.objMsg.reply("Unknown User"))
+				}
 				return reject(err);
 			});
 		}
