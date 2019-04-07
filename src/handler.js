c6a3a12c011d34b27b427664d3676870b56fe93c
Imp <georgehm@pm.me>
2019-04-07 20:53:56 +0100

    Update handler to use series, making a (hopefully) clearer structure

--- src/handler.js
+++ src/handler.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const modCommandList = require("./commands/index.js");
 const g_objCommandTemplate = JSON.parse(fs.readFileSync("./command_list.json", "utf8"));
 const g_objConfig = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+const modSeries = require("async-series");
 
 /**
  * Checks if a message is a command, then runs the command if its valid
@@ -13,72 +14,70 @@ const g_objConfig = JSON.parse(fs.readFileSync("./config.json", "utf8"));
  */
 module.exports.find = (strPrefix, objMsg, bot) => {
 	return new Promise((resolve, reject) => {
-		if (objMsg.content.startsWith(strPrefix)) {
-			// get the actual command function from fnFetchCommand
-			fnFetchCommand(objMsg.content.split(strPrefix)[1].split(" ")[0]).then(([objCommandData, fnCommand]) => {
-				// is this a function we can use on a message event?
-				if (objCommandData.on != "message")
-					return resolve();
+		if (!objMsg.content.startsWith(strPrefix)) {
+			return;
+		}
+		// get the actual command function from fnFetchCommand
+		fnFetchCommand(objMsg.content.split(strPrefix)[1].split(" ")[0]).then(([objCommandData, fnCommand]) => {
+			// going to set context later, 2 functions in the series use this
+			// didnt want to use a waterfall, so initing var outside of series works
+			let context;
 
-				// log data if an admin command is ran, also deny non-admins
-				if (objCommandData.admin == true) {
-					let bIsAdmin = g_objConfig.admins.includes(objMsg.author.id)
-
-					bot.db.collection("admin_log").insertOne(
-						{
-							admin: g_objConfig.admins.includes(objMsg.author.id),
-							user_id: objMsg.author.id,
-							username: objMsg.author.username,
-							discrim: objMsg.author.discriminator,
-							command: objCommandData.desc[1].replace("{PREFIX}").replace(" {ARGS}"),
-							where: objMsg.channel,
-							timestamp: Date.now()
-						}
-					);
-
-					if (!isAdmin) {
-						return resolve(objMsg.react("⛔"));
-					}	
-				}
-				// create context to pass (this contains our args, we pass objMsg by default)
-				let context = {
-					objMsg: objMsg, 
-					bot: bot,
-					objCommandTemplate: g_objCommandTemplate,
-					prefix: g_objConfig.prefix
-				};
-				// the template args
-				let arrTemplateArgs = objCommandData.args;
-				
-				context = fnAssembleArgs(objMsg.content, objCommandData, context);
-
-
-				// return the command
-				fnCommand(context).then(() => {
-					objMsg.react("✅");
-				}).catch(err => {
-					if (err === "help") {
-						let strComHelp = [
-							"Something went wrong trying to run your command, see the help: ```md",
-							fnCommandHelp(objCommandData),
-							"```"
-						].join("\n");
-						return reject(objMsg.reply(strComHelp));
+			modSeries([
+				function checkData(callback) {
+					if (objCommandData.on != "message") {
+						return resolve();
 					}
 
-					console.log(err);
-				});
-				// TODO: check args, pass args, command returns err if wrong args
-				// if wrong args return description (all commands have callback?)
-				// 
-				// need to check if command is on something other than "message"
-				// check if admin etc.
-			}).catch(err => {
-				return; //console.log(err);
-			});
-		}
+					if (objCommandData.admin == true) {
+						let bIsAdmin = fnCheckAdmin(
+							g_objConfig.admins,
+							objMsg,
+							objCommandData,
+							bot
+						);
 
-		return;
+						if (!bIsAdmin) {
+							return resolve(objMsg.react("⛔"));
+						}
+					}
+					return callback();
+				},
+				function createContext(callback) {
+					// create context to pass (this contains our args, we pass objMsg by default)
+					context = {
+						objMsg: objMsg, 
+						bot: bot,
+						objCommandTemplate: g_objCommandTemplate,
+						prefix: g_objConfig.prefix
+					};
+					// the template args
+					let arrTemplateArgs = objCommandData.args;
+					context = fnAssembleArgs(objMsg.content, objCommandData, context);
+
+					return callback();
+				},
+				function runCommand(callback) {
+					// return the command
+					fnCommand(context).then(() => {
+						objMsg.react("✅");
+					}).catch(err => {
+						if (err === "help") {
+							let strComHelp = [
+								"Something went wrong trying to run your command, see the help: ```md",
+								fnCommandHelp(objCommandData),
+								"```"
+							].join("\n");
+							return resolve(objMsg.reply(strComHelp));
+						}
+						return reject(err);
+					});
+				}
+			]);
+		}).catch(err => {
+			console.log(err);
+			return; //console.log(err);
+		});
 	});
 }
 
@@ -145,7 +144,6 @@ function fnCommandHelp(objCommandData) {
 
 	return arrRetFormat.join("\n");
 }
-
 
 
 /**
