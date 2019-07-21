5a6f2c183a0c5e304abbc5d9b0c50cf321775374
Imp <georgehm@pm.me>
2019-07-21 19:32:37 +0100

    Spaces to tabs

--- src/models/database.js
+++ src/models/database.js
@@ -2,55 +2,55 @@ const mongo = require("mongodb").MongoClient;
 
 class Database {
 
-    /**
-     * Connect to a mongodb database
-     *
-     * @param {String} host    The host to connect to
-     * @param {String} dbName  The name of the database to use
-     * @static
-     * @returns {Object}       Database connection
-     * @memberof Database
-     */
-    static async connect(host, dbName) {
-        try {
-            this._db = await mongo.connect(
-                host,
-                {useNewUrlParser:true}
-            );
+	/**
+	 * Connect to a mongodb database
+	 *
+	 * @param {String} host    The host to connect to
+	 * @param {String} dbName  The name of the database to use
+	 * @static
+	 * @returns {Object}       Database connection
+	 * @memberof Database
+	 */
+	static async connect(host, dbName) {
+		try {
+			this._db = await mongo.connect(
+				host,
+				{useNewUrlParser:true}
+			);
 
-            this._dbName = dbName;
-        } catch (err) {
-            throw err;
-        }
-    }
+			this._dbName = dbName;
+		} catch (err) {
+			throw err;
+		}
+	}
 
-    /**
-     * Get the current database connection
-     *
-     * @static
-     * @returns
-     * @memberof Database
-     */
-    static get(colName) {
-        if (!this._db) {
-            return undefined;
-        }
-        return this._db.db(this._dbName).collection(colName);
-    }
+	/**
+	 * Get the current database connection
+	 *
+	 * @static
+	 * @returns
+	 * @memberof Database
+	 */
+	static get(colName) {
+		if (!this._db) {
+			return undefined;
+		}
+		return this._db.db(this._dbName).collection(colName);
+	}
 
-    /**
-     * Close the database connection
-     *
-     * @static
-     * @memberof Database
-     */
-    static async disconnect() {
-        if (!this._db) {
-            return;
-        }
+	/**
+	 * Close the database connection
+	 *
+	 * @static
+	 * @memberof Database
+	 */
+	static async disconnect() {
+		if (!this._db) {
+			return;
+		}
 
-        await this._db.close();
-    }
+		await this._db.close();
+	}
 }
 
 module.exports = Database;
--- src/models/user.js
+++ src/models/user.js
@@ -6,116 +6,116 @@ const adminCollection = "admin_log";
 
 class User {
 
-    constructor(msg) {
-        this._id = msg.author.id;
-        this._username = msg.author.username;
-        this._discrim = msg.author.discriminator;
-        this._msg = msg;
-    }
+	constructor(msg) {
+		this._id = msg.author.id;
+		this._username = msg.author.username;
+		this._discrim = msg.author.discriminator;
+		this._msg = msg;
+	}
 
-    /**
-     * Saves some useful data to the dababase
-     *
-     * @memberof User
-     */
-    async save() {
-        let db = database.get(userCollection);
-        if (!db) {
-            return;
-        }
+	/**
+	 * Saves some useful data to the dababase
+	 *
+	 * @memberof User
+	 */
+	async save() {
+		let db = database.get(userCollection);
+		if (!db) {
+			return;
+		}
 
-        try {
-            let channel = this._msg.channel;
-            await db.updateOne(
-                {_id:this.getId()},
-                {
-                    $set: {
-                        username: this.getUsername(),
-                        discriminator:this.getDiscrim()
-                    },
-                    $push: {
-                        message: {
-                            content:this._msg.content,
-                            timestamp: Math.floor(Date.now()),
-                            where: channel.id
-                        }
-                    }
-                },
-                {upsert:true}
-            );
-        } catch (err) {
-            throw "Database: " + err;
-        }
-    }
+		try {
+			let channel = this._msg.channel;
+			await db.updateOne(
+				{_id:this.getId()},
+				{
+					$set: {
+						username: this.getUsername(),
+						discriminator:this.getDiscrim()
+					},
+					$push: {
+						message: {
+							content:this._msg.content,
+							timestamp: Math.floor(Date.now()),
+							where: channel.id
+						}
+					}
+				},
+				{upsert:true}
+			);
+		} catch (err) {
+			throw "Database: " + err;
+		}
+	}
 
-    /**
-     * Saves data to the admin log
-     * console logs if a user isn't admin
-     *
-     * @param {String} command  The command used
-     * @returns
-     * @memberof User
-     */
-    async adminLog(command) {
-        if (!this.isAdmin()) {
-            console.log(
-                this.getUsername() +
-                this.getDiscrim() +
-                " attempted to use admin command: " +
-                command
-            );
-        }
-        let db = database.get(adminCollection);
-        if (!db) {
-            return;
-        }
+	/**
+	 * Saves data to the admin log
+	 * console logs if a user isn't admin
+	 *
+	 * @param {String} command  The command used
+	 * @returns
+	 * @memberof User
+	 */
+	async adminLog(command) {
+		if (!this.isAdmin()) {
+			console.log(
+				this.getUsername() +
+				this.getDiscrim() +
+				" attempted to use admin command: " +
+				command
+			);
+		}
+		let db = database.get(adminCollection);
+		if (!db) {
+			return;
+		}
 
-        try {
-            let channel = this._msg.channel;
-            await db.updateOne(
-                {_id:this.getId()},
-                {
-                    $set: {
-                        admin: this.isAdmin()
-                    },
-                    $push: {
-                        message: {
-                            command: command,
-                            content:this._msg.content,
-                            timestamp: Math.floor(Date.now()),
-                            where: channel.id
-                        }
-                    }
-                },
-                {upsert:true}
-            );
-        } catch (err) {
-            console.log("Database: " + err);
-        }
-    }
+		try {
+			let channel = this._msg.channel;
+			await db.updateOne(
+				{_id:this.getId()},
+				{
+					$set: {
+						admin: this.isAdmin()
+					},
+					$push: {
+						message: {
+							command: command,
+							content:this._msg.content,
+							timestamp: Math.floor(Date.now()),
+							where: channel.id
+						}
+					}
+				},
+				{upsert:true}
+			);
+		} catch (err) {
+			throw "Database: " + err;
+		}
+	}
 
-    getId() {
-        return this._id;
-    }
+	getId() {
+		return this._id;
+	}
 
-    getUsername() {
-        return this._username;
-    }
+	getUsername() {
+		return this._username;
+	}
 
-    getDiscrim() {
-        return this._discrim;
-    }
+	getDiscrim() {
+		return this._discrim;
+	}
 
-    /**
-     * Checks if the user is an admin
-     *
-     * @returns {Boolean} True if admin - false if not
-     * @memberof User
-     */
-    isAdmin() {
-        let id = this.getId();
-        return id.includes(config.admins);
-    }
+	/**
+	 * Checks if the user is an admin
+	 *
+	 * @returns {Boolean} True if admin - false if not
+	 * @memberof User
+	 */
+	isAdmin() {
+		let id = this.getId();
+		return id.includes(config.admins);
+	}
 }
 
 module.exports = User;
